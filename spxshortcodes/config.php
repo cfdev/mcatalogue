f8f00e09bb15899ff65af9357c76ad0565d72c27
Cyril <cfdev161@gmail.com>
2015-10-08 10:33:23 +0200

    Ajout du support des shortcodes
    [MCATALOGUE CAT=001]

---
+++ spxshortcodes/config.php
@@ -0,0 +1,27 @@
+<?php 
+/**
+	Author: cfdev
+	website: http://cfdev.fr/
+	Shortcodes plugin: http://secretsitebox.fr/site/index.php?categorie2/pluxml-plugins#post-10/
+	
+*/     
+   
+   
+/**
+*/
+function mcatalogue_func( $atts, $content = null ) {
+
+	global $plxShow;
+	
+	extract( shortcode_atts( array(
+      'cat' => 'cat',
+	  
+      ), $atts ) );
+	  
+
+	return $plxShow->callHook('mcatalogueShow',$cat);
+}
+
+add_shortcode('MCATALOGUE', 'mcatalogue_func');
+
+?>
